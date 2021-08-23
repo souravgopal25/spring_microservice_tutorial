d00558fe02a945839b3bab50bffd5616eb852a55
Sourav <souravgopal25@gmail.com>
2021-08-24 00:59:19 +0530

    Endpoints Created for movie-info-service

--- movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,6 +2,9 @@ package io.javabrains.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class MovieInfoServiceApplication {
