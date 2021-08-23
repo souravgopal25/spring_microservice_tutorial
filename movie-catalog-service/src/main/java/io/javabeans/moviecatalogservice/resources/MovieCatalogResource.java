0b46b1e07c47c520f95259b9be2723ea6f907f1f
Sourav <souravgopal25@gmail.com>
2021-08-23 23:49:36 +0530

    endpoints created with dummy data in movie-catalog-service

--- movie-catalog-service/src/main/java/io/javabeans/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabeans/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,7 +1,20 @@
 package io.javabeans.moviecatalogservice.resources;
 
+import io.javabeans.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+    @GetMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(new CatalogItem("Transformers", "test", 4));
+
+    }
 }
