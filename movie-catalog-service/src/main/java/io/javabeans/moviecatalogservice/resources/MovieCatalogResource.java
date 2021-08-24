53b1f9c4ea96f656b1f503116bedd00af7358725
Sourav <souravgopal25@gmail.com>
2021-08-25 05:20:28 +0530

    used Rest Template to get resources from Movie Info Service

--- movie-catalog-service/src/main/java/io/javabeans/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabeans/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,24 +1,39 @@
 package io.javabeans.moviecatalogservice.resources;
 
 import io.javabeans.moviecatalogservice.models.CatalogItem;
+import io.javabeans.moviecatalogservice.models.Movie;
+import io.javabeans.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123", 4),
+                new Rating("5678", 5)
+        );
+        RestTemplate restTemplate = new RestTemplate();
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movie/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
         //get all rated movie ID's
         //For each movie Id, call movie info service and get details
 
         //put them all together
-        return Collections.singletonList(new CatalogItem("Transformers", "test", 4));
+        //  return Collections.singletonList(new CatalogItem("Transformers", "test", 4));
 
     }
 }
