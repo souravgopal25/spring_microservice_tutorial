b40159bcd48a2540c02a670a45c2eddfc7c9c2c4
Sourav <souravgopal25@gmail.com>
2021-10-03 13:18:44 +0530

    Role Based Authentication +Authorization

---
+++ springsecurity/src/main/java/com/sourav/security/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.sourav.security.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+    static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
